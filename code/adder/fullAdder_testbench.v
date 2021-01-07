86c143db681d43938f84571f711bbc46ef67bf23
mozerpol <mozerpol@protonmail.com>
2021-01-07 18:40:31 +0100

    Add comments to testbench

--- code/adder/fullAdder_testbench.v
+++ code/adder/fullAdder_testbench.v
@@ -1,6 +1,7 @@
-module fullAdder_tb;
+module fullAdder_tb;// if it's testbech it is worth adding "_tb"
+  // variable declaration, which are using in our testbench
   
-  integer i;
+  integer i; // For loop purposes 
   reg A_tb, B_tb, Cin_tb;
   wire S_tb, Cout_tb;
   
@@ -24,11 +25,9 @@ module fullAdder_tb;
       for(i = 1; i < 9; i = i+1)
         begin
           #5
-          A_tb = 1^A_tb;
-          B_tb = i>>1&1;
-          Cin_tb = i>>2&1;
+          A_tb = 1^A_tb; // A_tb = 0101010101...
+          B_tb = i>>1&1; // B_tb = 001100110011...
+          Cin_tb = i>>2&1; // Cin_tb = 000011110000...
         end
-      
     end
-  
 endmodule
