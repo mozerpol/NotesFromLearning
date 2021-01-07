54e0888f4e3e1db368ace17422af29a87cb169c0
mozerpol <mozerpol@protonmail.com>
2021-01-07 16:17:11 +0100

    Add comments to halfAdder files

--- code/adder/halfAdder_testbench.v
+++ code/adder/halfAdder_testbench.v
@@ -1,10 +1,10 @@
-module halfAdder_tb;
-  
+module halfAdder_tb; // if it's testbech it is worth adding "_tb"
+  // variable declaration, which are using in our testbench
   integer i;
-  reg A_tb, B_tb;
-  wire C_tb, S_tb;
+  reg A_tb, B_tb; // If inputs in our model are wire type, then variables in testbench should be reg type
+  wire C_tb, S_tb; // If we want connect output from our model, then variables in testbench should be wire type
   
-  halfAdder UUT(
+  halfAdder UUT( // binding vars from test with model
     .A(A_tb),
     .B(B_tb),
     .C(C_tb),
@@ -16,10 +16,10 @@ module halfAdder_tb;
       $dumpfile("tb.vcd");
       $dumpvars;
 
-      A_tb <= $random;
+      A_tb <= $random; // Assign random number from 0 to 1 to var
       B_tb <= $random;
       
-      for(i = 1; i < 10; i = i+1)
+      for(i = 1; i < 10; i = i+1) // Count up to ten, because will be more possibilities in our waveform
         begin
           #5 
           A_tb <= $random;
