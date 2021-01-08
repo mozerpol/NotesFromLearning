4dcc1be1968a76b65e6e8dd9fdf464aa97eb8c8b
mozerpol <mozerpol@protonmail.com>
2021-01-08 17:10:36 +0100

    Add comment to design

--- code/alu/oneBitALU_design.v
+++ code/alu/oneBitALU_design.v
@@ -1,11 +1,12 @@
 module ALU_1_bit(
   input a,
   input b, 
-  input [1:0] operation,
+  input [1:0] operation, // We have four possible operations, so we need two bits,
+  						 // 2^2 = 4. 4 = four operations
   output reg result
 );
 
-  always@(a or b or operation)
+  always@(a or b or operation) // @ - this is event control.
     begin
       case (operation)
         2'b00:  result = a & b; // AND
