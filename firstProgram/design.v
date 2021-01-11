83a9eabfc52c78d25fbf66ad2dc4e4a499e6a802
mozerpol <mozerpol@protonmail.com>
2021-01-12 00:16:44 +0100

    Add comments

--- firstProgram/design.v
+++ firstProgram/design.v
@@ -1,22 +1,29 @@
 module test();
 
-wire out1;
-reg in1, in2; 
-  nand U1(out1, in1, in2);
+wire out1; // wire - network of connections between components in a digital circuit.
+// They are 1-bit wide and default value is "z". If we don't define type, then default
+// will be wire.
+reg in1, in2; // reg - registers, it's used for modeling of memory elements.
+  nand U1(out1, in1, in2); // The outputs are as the first argument. U1 - name of gate.
 
-initial begin 
+initial begin // Behavioral level, there are two types of blocks at this level: initial and 
+  // always. Inside these blocks, the instructions are executed sequentially.
   $dumpfile("tb.vcd"); 
   $dumpvars;
-	$monitor("wejscie1 = %b wejscie2 = %b wyjscie = %b", in1, in2, out1);
+	$monitor("input1 = %b input2 = %b output = %b", in1, in2, out1);
 	in1 = 0;
 	in2 = 0;
-	#1 in1 = 1;
+	#1 in1 = 1; // 1 sec delay from previous instruction. It's possible because 
+	// the instructions in the initial block are executed sequentially, one after the other.
 	in2 = 1;
 	#1 in1 = 0; 
   	#2 in1 = 1;
   	in2 = 0;
-	#10 $finish;
-end
+	#10 $finish; // $ - from this system commands begin. finish - it stop simpulation
+end // begin, end - grouping commands, something like braces in C.
 
 endmodule
+
+
+
 
