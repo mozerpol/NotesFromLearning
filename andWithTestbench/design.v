032ecd4cfc016ad310a4f636478ad6c176077f65
mozerpol <mozerpol@protonmail.com>
2021-01-12 00:40:41 +0100

    Add comments to design

--- andWithTestbench/design.v
+++ andWithTestbench/design.v
@@ -1,5 +1,5 @@
 module gate(
-  input wire in1,
+  input wire in1, // See desc at the bottom
   input wire in2,
   output wire out1
 );
@@ -7,3 +7,7 @@ module gate(
   and (out1, in1, in2);
   
 endmodule
+/* The rules to which signals we can connect the ports of the module:
+    1. input - it can be wire, but to external module should connect to reg or wire
+    2. output - it can be wire or reg, in the external module must be wire.
+*/
