9041c4b31bf5b646599336dafae554403b19418b
mozerpol <mozerpol@protonmail.com>
2021-01-13 20:11:07 +0100

    Add comments

--- twoEmbeddedModules/design.v
+++ twoEmbeddedModules/design.v
@@ -1,11 +1,17 @@
-module comb_simple(a1_i, a2_i, a3_o);
-  
-  input a1_i, a2_i;
+module comb_simple( // after keyword "module" is its name and portlist, thanks to 
+// which you can communicate with the environment. If we don't define type, then
+// default will be wire.
+    a1_i, 
+    a2_i, 
+    a3_o
+);
+  input a1_i, a2_i; // If we don't define type, then default will be wire.
   output a3_o;
   wire tmp;
-  not N1(tmp, a2_i);
+  not N1(tmp, a2_i); // The outputs are as the first argument. N1 - name of gate.
   and A1(a3_o, a1_i, tmp);
-  
+  // Different way for this exercise, it's register level
+  // assign a3_o = a1_i & ~a2_i;
 endmodule
 
 module comb_complex(b1_i, b2_i, b3_i, b4_o);
@@ -17,3 +23,16 @@ module comb_complex(b1_i, b2_i, b3_i, b4_o);
   comb_simple M2(internal_wire, b3_i, b4_o);
   
 endmodule
+
+/*
+The action of this is easy:
+We have exactly two the same modules: comb_simple, which are located inside the
+larger one, comb_complex. comb_simple has two inputs (a1_i and a2_i) and two
+gates connected in series: and, not. The output of first comb_simple go to input
+second comb_simple.
+*/
+
+
+
+
+
--- twoEmbeddedModules/testbench.v
+++ twoEmbeddedModules/testbench.v
@@ -1,11 +1,11 @@
-module comb_complex_tb();
+module comb_complex_tb(); // it's good to add "_tb"
   
-  reg b1_i_tb;
+  reg b1_i_tb; // must be reg type, because in our model b1_i is wire type
   reg b2_i_tb;
   reg b3_i_tb;
-  wire b4_o_tb;
+  wire b4_o_tb; // must be wire, because it's output port
   
-  comb_complex UUT1(
+  comb_complex UUT1( // UUT is an instance of the gate module. UUT - unit under test
     .b1_i(b1_i_tb),
     .b2_i(b2_i_tb),
     .b3_i(b3_i_tb),
@@ -13,7 +13,8 @@ module comb_complex_tb();
   );
   
   initial begin
-   	$dumpfile("tb.vcd"); 
+   	$dumpfile("tb.vcd"); // $dumpfile - should be given at the beginning to
+    // monitor the state of the variables from beginning.
     $dumpvars;
     $display("\t\t       b1_i | b2_i| b3_i| b4_o");
     $monitor($time, "  |  %d  |  %d  |  %d  |  %d", b1_i_tb, b2_i_tb, b3_i_tb, b4_o_tb);
@@ -41,7 +42,9 @@ module comb_complex_tb();
     b1_i_tb = 1;
     b2_i_tb = 1;
     b3_i_tb = 1;
-    
+    // In this case we aren't add $finish command ($finish ends simulation,
+    // dumpfile operation and variable monitoring), because we don't have
+    // infinite loop. In this testbench the code will run sequentially.
   end
   
 endmodule
