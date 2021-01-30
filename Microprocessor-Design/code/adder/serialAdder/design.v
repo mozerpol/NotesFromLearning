73098670306469628bca074ac79fb20dfc09d905
mozerpol <mozerpol@protonmail.com>
2021-01-30 16:50:37 +0100

    Add comments

--- Microprocessor-Design/code/adder/serialAdder/design.v
+++ Microprocessor-Design/code/adder/serialAdder/design.v
@@ -1,10 +1,11 @@
-module serial_adder(   
+module serial_adder(// after keyword "module" is its name and portlist, thanks to 
+// which you can communicate with the environment.   
   input clk,
   input in_A, in_B,
   output out
 );
 
-  reg flag = 0;
+  reg flag = 0; // internal variable declaration
   reg out = 0;
 
   always@(posedge clk)
