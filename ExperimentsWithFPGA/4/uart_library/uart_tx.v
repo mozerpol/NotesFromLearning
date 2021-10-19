9b616c4a2c6213264e8b499ff38dcba7c351b1ae
mozerpol <krzmozer@student.pg.edu.pl>
2021-10-19 16:48:48 +0200

    Add start and stop bit to data frame

--- ExperimentsWithFPGA/4/uart_library/uart_tx.v
+++ ExperimentsWithFPGA/4/uart_library/uart_tx.v
@@ -12,30 +12,48 @@ module uart_tx
       output wire tx
    );
 
-   // Data sending part
-   reg [7:0] data_reg;
-   assign data_reg = data;
    wire tx_clk;
-   reg [2:0] i;
+   reg rst_reg;
+   assign rst_reg = rst;
+   reg flag;
+   reg [3:0] i;
 
-   // Generate clock cycle for data transmission
+   // Generate clock cycle for data transmission (baud rate)
    counter #(.N(8)) clkTx (
       .clk(clk),
-      .rst(rst),
+      .rst(!rst),
       .ce(1'b1),
       .q(),
       .ov(tx_clk)
    );
    
-   // Generate clock for baud rate 
-   counter #(.N(8)) clkBaud (
+   // Go through array and send data 
+   counter #(.N(10)) clkBaud (
       .clk(clk),
-      .rst(rst),
+      .rst(!rst),
       .ce(tx_clk),
       .q(i),
       .ov()
    );
 
-   assign tx = data_reg[i];
-
+   reg [9:0] full_frame;
+   reg [7:0] prev_full_frame;
+   assign full_frame[0] = 1'b0; // Start bit
+   assign full_frame[9] = 1'b1; // Stop bit
+   assign full_frame[8:1] = data;
+   assign tx = full_frame[i];
+/*
+   always@(posedge clk)
+   begin
+      if(flag == 1'b1) begin
+         rst_reg <= 1'b0;
+         flag <= 1'b0;
+      end
+      if(full_frame[8:1] != data) 
+      begin
+         rst_reg <= 1'b1;
+         flag <= 1'b1;
+      end
+   end
+*/
 endmodule
