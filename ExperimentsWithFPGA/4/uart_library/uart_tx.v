57976391c82be2965ba33e217a59fedd98414fbc
mozerpol <krzmozer@student.pg.edu.pl>
2021-10-22 15:32:24 +0200

    Add new tests

--- ExperimentsWithFPGA/4/uart_library/uart_tx.v
+++ ExperimentsWithFPGA/4/uart_library/uart_tx.v
@@ -7,17 +7,14 @@ module uart_tx
       output wire tx
    );
 
-   wire tx_clk;
    wire [3:0] i;
-   reg stop_counter = 1'b1;
-   reg ov_clkBaud = 1'b0;
-   reg rst_cnt = 1'b0;
-
+   wire ov_clkBaud;
+   
    // Generate clock cycle for data transmission (baud rate)
    counter #(.N(1000)) clkTx (
       .clk(clk),
-      .rst(!(rst || rst_cnt)),
-      .ce(stop_counter),
+      .rst(!rst),
+      .ce(1'b1),
       .q(),
       .ov(tx_clk)
    );
@@ -25,35 +22,36 @@ module uart_tx
    // Go through array and send data 
    counter #(.N(10)) clkBaud (
       .clk(clk),
-      .rst(!(rst || rst_cnt)),
+      .rst(!rst),
       .ce(tx_clk),
       .q(i),
       .ov(ov_clkBaud)
    );
 
-   wire [7:0] ascii_data;
-   assign ascii_data = 8'd48 + data;
    wire [9:0] full_frame;
    assign full_frame[0] = 1'b0; // Start bit
    assign full_frame[9] = 1'b1; // Stop bit
-   assign full_frame[8:1] = ascii_data;
-   assign tx = full_frame[i];
-   reg [7:0] old_data;
-   reg flag = 1'b1;
+   assign full_frame[8:1] = data;//ascii_data;
+   reg [7:0] old_data = 8'b00000000;
+   reg tx_reg;
+   assign tx = tx_reg;
+   reg state;
 
-   always@(posedge clk)
-   begin
-      if(flag && ov_clkBaud) begin // Latch current value
+   always@(posedge clk) begin
+      if(data != old_data) begin
          old_data <= data;
-         flag <= 1'b0;
-         stop_counter <= 1'b0;
-         rst_cnt <= 1'b1;
+         state <= 1'b0;
       end
-      if(old_data^data) begin
-         flag <= 1'b1; // Value has changed
-         stop_counter <= 1'b1;
-         rst_cnt <= 1'b0;
+			if(ov_clkBaud) begin
+         state <= 1'b1;
       end
+   end
+
+   always@(posedge clk) begin
+      case(state)
+         1'b0: tx_reg <= full_frame[i];
+         1'b1: tx_reg <= full_frame[9];
+      endcase
    end
 
 endmodule
