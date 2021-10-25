b0956946da66ddffa6faa80cb25abe9f9cb57df6
mozerpol <krzmozer@student.pg.edu.pl>
2021-10-25 18:50:34 +0200

    Add restet after overflow cnt1 and cnt2

--- ExperimentsWithFPGA/4/uart_library/uart_tx.v
+++ ExperimentsWithFPGA/4/uart_library/uart_tx.v
@@ -9,11 +9,13 @@ module uart_tx
 
    wire [3:0] i;
    wire ov_clkBaud;
+   reg rst_cnt1 = 1'b0;
+   reg rst_cnt2 = 1'b0;
    
    // Generate clock cycle for data transmission (baud rate)
    counter #(.N(1000)) clkTx (
       .clk(clk),
-      .rst(!rst),
+      .rst(!(rst_cnt1 || rst)),
       .ce(1'b1),
       .q(),
       .ov(tx_clk)
@@ -22,7 +24,7 @@ module uart_tx
    // Go through array and send data 
    counter #(.N(10)) clkBaud (
       .clk(clk),
-      .rst(!rst),
+      .rst(!(rst_cnt2 || rst)),
       .ce(tx_clk),
       .q(i),
       .ov(ov_clkBaud)
@@ -33,9 +35,11 @@ module uart_tx
    assign full_frame[9] = 1'b1; // Stop bit
    assign full_frame[8:1] = data;//ascii_data;
    reg [7:0] old_data = 8'b00000000;
-   reg tx_reg;
+   reg tx_reg = 1'b1;
    assign tx = tx_reg;
    reg state;
+
+// ADD rst_cnt1
 
    always@(posedge clk) begin
       if(data != old_data) begin
@@ -44,13 +48,23 @@ module uart_tx
       end
 			if(ov_clkBaud) begin
          state <= 1'b1;
-      end
+         end 
    end
 
    always@(posedge clk) begin
       case(state)
-         1'b0: tx_reg <= full_frame[i];
-         1'b1: tx_reg <= full_frame[9];
+         1'b0: 
+            begin
+               tx_reg <= full_frame[i];
+               rst_cnt2 <= 1'b0;
+               rst_cnt1 <= 1'b0;
+            end
+         1'b1: 
+            begin
+               tx_reg <= full_frame[9];
+               rst_cnt2 <= 1'b1;
+               rst_cnt1 <= 1'b1;
+            end
       endcase
    end
 
