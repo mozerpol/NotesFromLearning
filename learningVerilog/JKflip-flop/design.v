d13d42ba9bbd41880a82cdb18b78229ff9964c85
mozerpol <krzmozer@student.pg.edu.pl>
2021-11-08 20:06:34 +0100

    Add code to jk flip-flop

--- learningVerilog/JKflip-flop/design.v
+++ learningVerilog/JKflip-flop/design.v
@@ -4,4 +4,17 @@ module jkff(
    input wire k,
    output wire q
 );
+
+   reg q_reg;
+   assign q = q_reg;
+
+   always@(posedge clk) begin
+      case({j,k})
+         2'b00: q_reg <= q_reg;
+         2'b01: q_reg <= 1'b0; 
+         2'b10: q_reg <= 1'b1;
+         2'b11: q_reg <= ~q_reg;
+      endcase
+   end
+
 endmodule
