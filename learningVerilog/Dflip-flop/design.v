a4a6b00fff213dc2a3fbad53da491a9d31d3f8fb
mozerpol <krzmozer@student.pg.edu.pl>
2021-11-08 13:22:06 +0100

    Add comments

--- learningVerilog/Dflip-flop/design.v
+++ learningVerilog/Dflip-flop/design.v
@@ -1,3 +1,6 @@
+/* The act of D flip-flop is very simple. When we have rising edge on clk then
+* it accepts data from d input and rewrite this input to the output.
+*/
 module dff(
    input wire clk,
    input wire d,
