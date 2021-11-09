b34882baadfe4bee83a6f3857873a85f00ac115b
mozerpol <krzmozer@student.pg.edu.pl>
2021-11-09 20:12:36 +0100

    Add main functionality

--- learningVerilog/SRflip-flop/design.v
+++ learningVerilog/SRflip-flop/design.v
@@ -5,6 +5,16 @@ module srff(
    output wire q
 );
 
+   reg q_reg;
+   assign q = q_reg;
 
+   always@(posedge clk) begin
+      case({s,r})
+         2'b00: q_reg <= q_reg; 
+         2'b01: q_reg <= 1'b0;
+         2'b10: q_reg <= 1'b1;
+         2'b11: q_reg <= 1'bx;
+      endcase
+   end
 
 endmodule
