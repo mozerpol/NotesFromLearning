fcf843e3929a27c76af203140b7af9dd8d084020
mozerpol <krzmozer@student.pg.edu.pl>
2021-11-09 22:36:05 +0100

    Add main functionality

--- learningVerilog/Tflip-flop/design.v
+++ learningVerilog/Tflip-flop/design.v
@@ -3,4 +3,13 @@ module tff(
    input wire t,
    output wire q
 );
+   
+   reg q_reg = 1'b0;
+   assign q = q_reg;
+   
+   always@(posedge clk) begin
+      if(t) q_reg <= ~q_reg;
+      else q_reg <= q_reg;
+   end
+
 endmodule
