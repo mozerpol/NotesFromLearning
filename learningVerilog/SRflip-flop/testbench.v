76215d2d59357520c6829116a4a884b2f4f0666d
mozerpol <krzmozer@student.pg.edu.pl>
2021-11-09 20:14:48 +0100

    Add tests to test bench

--- learningVerilog/SRflip-flop/testbench.v
+++ learningVerilog/SRflip-flop/testbench.v
@@ -1,3 +1,5 @@
+`timescale 100ns/100ns
+
 module srff_tb();
 
    reg clk = 1'b1;
@@ -7,7 +9,13 @@ module srff_tb();
    srff dut(clk, s, r, q);
 
    initial begin
+      #10 s = 1'b1; r = 1'b0;
+      #20 s = 1'b0; r = 1'b1;
+      #20 s = 1'b0; r = 1'b0;
+      #20 s = 1'b1; r = 1'b1;
       #40 $stop;
    end
+
+   always #10 clk <= ~clk;
 
 endmodule
