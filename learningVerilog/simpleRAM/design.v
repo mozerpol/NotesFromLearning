efaf15f23e05769e7479210405b166daf753dc08
mozerpol <krzmozer@student.pg.edu.pl>
2021-11-11 19:31:47 +0100

    Add memory declaration

--- learningVerilog/simpleRAM/design.v
+++ learningVerilog/simpleRAM/design.v
@@ -3,8 +3,20 @@ module simpleRAM #(
    parameter addressSize = 32
 )(
    input wire clk,
-   input wire we,
-   input wire re,
+   input wire we, // Write enable
+   input wire re, // Read enable
    inout wire data
 );
+
+   reg [wordSize-1:0] memory [0:addressSize-1];
+   reg ce;
+
+   assign memory[0][7:0] = 8'h1;
+   assign memory[1][7:0] = 8'h2;
+   assign memory[2][7:0] = 8'h3;
+
+   always@(posedge clk) begin
+      if(we&re); // do sth
+   end
+
 endmodule
