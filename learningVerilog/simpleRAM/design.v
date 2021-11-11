222fd0cc03f0301b86f67b982c89b888d33f7410
mozerpol <krzmozer@student.pg.edu.pl>
2021-11-11 22:56:27 +0100

    Add possibility of simple saving to memory

--- learningVerilog/simpleRAM/design.v
+++ learningVerilog/simpleRAM/design.v
@@ -5,18 +5,24 @@ module simpleRAM #(
    input wire clk,
    input wire we, // Write enable
    input wire re, // Read enable
-   inout wire data
+   input wire addr,
+   inout wire [7:0] data
 );
 
    reg [wordSize-1:0] memory [0:addressSize-1];
    reg ce;
 
+   /*
    assign memory[0][7:0] = 8'h1;
    assign memory[1][7:0] = 8'h2;
    assign memory[2][7:0] = 8'h3;
+   */
 
    always@(posedge clk) begin
-      if(we&re); // do sth
+    //  if(we&re); // do sth
+      if(we) begin
+         memory[0][7:0] <= data;
+      end
    end
 
 endmodule
