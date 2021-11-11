a7e22c426550973f49bb691225860047bd3ed617
mozerpol <krzmozer@student.pg.edu.pl>
2021-11-11 22:56:56 +0100

    Bind test with design and add simple test

--- learningVerilog/simpleRAM/testbench.v
+++ learningVerilog/simpleRAM/testbench.v
@@ -4,11 +4,25 @@ module simpleRAM_tb;
 
    reg clk = 1'b1;
    reg we, re;
-   wire data;
+   wire [7:0] data;
+   reg [7:0] data_reg;
+   assign data = data_reg;
+
+   simpleRAM #(
+      .wordSize(8),
+      .addressSize(32)
+   ) uut (
+      clk, we, re, addr, data
+   );
 
    initial begin
+      we = 1'b1;
+      #10 data_reg = 8'h1;
+      #20 data_reg = 8'h2;
+
+      #100 $stop;
    end
 
-   always #10 clk = ~clk;
+   always #10 clk <= ~clk;
 
 endmodule
