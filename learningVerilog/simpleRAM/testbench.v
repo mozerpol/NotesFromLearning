e2dae6b5ffe7d6bb854d76225a28821283bbfef4
mozerpol <krzmozer@student.pg.edu.pl>
2021-11-12 12:48:57 +0100

    Assign in for loop variables to memory

--- learningVerilog/simpleRAM/testbench.v
+++ learningVerilog/simpleRAM/testbench.v
@@ -1,5 +1,7 @@
 `timescale 100ns/100ns
 
+`define ADDRESSSIZE 32 
+
 module simpleRAM_tb;
 
    reg clk = 1'b1;
@@ -7,22 +9,27 @@ module simpleRAM_tb;
    wire [7:0] data;
    reg [7:0] data_reg;
    assign data = data_reg;
+   integer i = 0;
+   reg [4:0] addr;
 
    simpleRAM #(
       .wordSize(8),
-      .addressSize(32)
+      .addressSize(`ADDRESSSIZE)
    ) uut (
       clk, we, re, addr, data
    );
 
    initial begin
-      we = 1'b1;
-      #10 data_reg = 8'h1;
-      #20 data_reg = 8'h2;
+      #10 we = 1'b1;
+
+      for(i = 0; i < (`ADDRESSSIZE-1); i = i+1) begin
+         #10 addr = i;
+         data_reg = 8'h1;
+      end
 
       #100 $stop;
    end
 
-   always #10 clk <= ~clk;
+   always #5 clk <= ~clk;
 
 endmodule
