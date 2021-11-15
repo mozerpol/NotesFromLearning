ee2ec6f0ad11918aca9403ef9ad792457bc52df0
mozerpol <krzmozer@student.pg.edu.pl>
2021-11-15 14:04:05 +0100

    Change wordsize and add test for reading

--- learningVerilog/simpleRAM/testbench.v
+++ learningVerilog/simpleRAM/testbench.v
@@ -1,6 +1,7 @@
 `timescale 100ns/100ns
 
-`define ADDRESSSIZE 32 
+`define ADDRESSSIZE 32
+`define WORDSIZE 8
 
 module simpleRAM_tb;
 
@@ -13,7 +14,7 @@ module simpleRAM_tb;
    reg [4:0] addr;
 
    simpleRAM #(
-      .wordSize(8),
+      .wordSize(`WORDSIZE),
       .addressSize(`ADDRESSSIZE)
    ) uut (
       clk, we, re, addr, data
@@ -21,13 +22,19 @@ module simpleRAM_tb;
 
    initial begin
       #10 we = 1'b1;
-
-      for(i = 0; i < (`ADDRESSSIZE-1); i = i+1) begin
+      re = 1'b0;
+      for(i = 0; i <= (`ADDRESSSIZE-1); i = i+1) begin
          #10 addr = i;
-         data_reg = 8'h1;
+         data_reg = {8{i}};
       end
 
-      #100 $stop;
+      #10 re = 1'b1;
+      we = 1'b0;
+      for(i = 0; i <= (`ADDRESSSIZE-1); i = i+1) begin
+         #10 addr = i;
+      end
+
+      #10 $stop;
    end
 
    always #5 clk <= ~clk;
