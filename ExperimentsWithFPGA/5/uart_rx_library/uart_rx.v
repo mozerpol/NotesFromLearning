97f40268f8eb726f64560236c9b9480132513106
mozerpol <krzmozer@student.pg.edu.pl>
2021-11-17 18:47:08 +0100

    Add two counters

--- ExperimentsWithFPGA/5/uart_rx_library/uart_rx.v
+++ ExperimentsWithFPGA/5/uart_rx_library/uart_rx.v
@@ -2,6 +2,33 @@ module uart_rx #(
    parameter F = 8000000,
    parameter BAUD = 115200
 ) (
-   input wire rx
+   input wire rx,
+   input wire clk,
+   input wire rst
 );
+   
+   wire [3:0] i;
+   wire ov_clkBaud;
+   reg rst_after_ov = 1'b0;
+   wire tx_clk; // Bring together clkTx with clkBaud
+   
+   // Generate clock cycle for data transmission (baud rate)
+   counter #(.N((F+BAUD/2)/BAUD)) clkTx (
+      .clk(clk),
+      .rst(!(rst_after_ov || rst)),
+      .ce(1'b1),
+      .q(),
+      .ov(tx_clk)
+   );
+   
+   // Go through array and send data 
+   counter #(.N(10)) clkBaud (
+      .clk(clk),
+      .rst(!(rst_after_ov || rst)),
+      .ce(tx_clk),
+      .q(i),
+      .ov(ov_clkBaud)
+   );
+
+
 endmodule
