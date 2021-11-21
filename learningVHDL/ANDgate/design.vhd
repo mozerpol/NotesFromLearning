25cf5aef509129b4846984f0efa402e625902fb2
mozerpol <krzmozer@student.pg.edu.pl>
2021-11-21 22:18:22 +0100

    Change name from design to testbench

--- learningVHDL/ANDgate/design.vhd
+++ learningVHDL/ANDgate/design.vhd
@@ -1,41 +1,16 @@
-library IEEE;
+library IEEE; 
 use IEEE.std_logic_1164.all;
 
-entity testbench is
-  end testbench;
+entity and_gate is port(
+  a: in std_logic;
+  b: in std_logic;
+  c: out std_logic;
+);
+end and_gate;
 
-  architecture tb of testbench is
-
-    component and_gate is
-      port(
-        a: in std_logic;
-        b: in std_logic;
-        c: out std_logic;
-      );
-    end component;
-
-      signal a_in, b_in, c_out: std_logic;
-
+architecture rtl of and_gate is
   begin
-
-    DUT: and_gate port map(a_in, b_in, c_out);
-
-process begin
-  a_in <= '0';
-  b_in <= '0';
-wait for 1 ns;
-  a_in <= '0';
-b_in <= '1';
-wait for 1 ns;
-  a_in <= '1';
-b_in <= 'X';
-wait for 1 ns;
-  a_in <= '1';
-b_in <= '1';
-wait for 1 ns;
-  -- Clear inputs
-  a_in <= '0';
-b_in <= '0';
-wait;
-end process;
-  end tb;
+    process(a, b) is begin
+      c <= a and b;
+    end process;
+end rtl;
