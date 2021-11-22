864842b33330e48e32ca73b951babc0db90f3c20
mozerpol <krzmozer@student.pg.edu.pl>
2021-11-22 21:55:04 +0100

    Add comments and fix stylistic errors

--- learningVHDL/ORgate/design.vhd
+++ learningVHDL/ORgate/design.vhd
@@ -1,21 +1,32 @@
 -- Simple OR gate design
+
 library IEEE; -- library defines how keywords behave in file
-use IEEE.std_logic_1164.all;
+  use IEEE.std_logic_1164.all;
 
 -- Below entity is called "or_gate"
-entity or_gate is -- entity contains a port that defines all inputs and outputs to a file.
-port(
-  a: in std_logic; -- std_logic - type
-  b: in std_logic;
-  q: out std_logic
-  );
-end or_gate;
 
--- architecture is used to describe the functionality of a particular entity
-architecture rtl of or_gate is -- architecture is called "rtl" of entity "or_gate" 
+entity or_gate is        -- entity contains a port that defines all inputs and outputs to a file.
+  port (
+    a : in    std_logic; -- std_logic - type
+    b : in    std_logic;
+    q : out   std_logic
+  );
+end entity or_gate;
+
+-- architecture is used to describe the functionality of a particular entity.
+-- It is possible to have multiple architecture blocks associated with one
+-- entity block, but only one architecture block is selected for a given entity
+-- during simulation or synthesis.
+
+architecture rtl of or_gate is -- architecture is called "rtl" of entity "or_gate"
+
 begin
-  process(a, b) is
-  begin
+
+  process_or : process (a, b) is
+  begin            -- Code inside the process statement is executed sequentially
+
     q <= a or b; -- "q" - it's output, "input_1", "input_2" - inputs.
-  end process;
-end rtl;
+
+  end process process_or;
+
+end architecture rtl;
--- learningVHDL/ORgate/testbench.vhd
+++ learningVHDL/ORgate/testbench.vhd
@@ -1,55 +1,80 @@
 -- Testbench for OR gate
+
 library IEEE;
-use IEEE.std_logic_1164.all;
- 
+  use IEEE.std_logic_1164.all;
+
 entity testbench is
--- empty
-end testbench; 
+  -- empty
+end entity testbench;
 
 architecture tb of testbench is
 
--- DUT component
-component or_gate is
-port(
-  a: in std_logic;
-  b: in std_logic;
-  q: out std_logic);
-end component;
+  -- DUT component, component <component_name>
+  component or_gate is
+    port (
+      a : in    std_logic;
+      b : in    std_logic;
+      q : out   std_logic
+    );
+  end component;
 
-signal a_in, b_in, q_out: std_logic;
+  signal a_in  : std_logic;
+  signal b_in  : std_logic;
+  signal q_out : std_logic;
 
 begin
 
   -- Connect DUT
-  DUT: or_gate port map(a_in, b_in, q_out);
+  dut : component or_gate
+    port map (
+      a => a_in,
+      b => b_in,
+      q => q_out
+    );
 
-  process
+  process_tb : process is
   begin
+
     a_in <= '0';
     b_in <= '0';
     wait for 1 ns;
-    assert(q_out='0') report "Fail 0/0" severity error;
-  
+    assert(q_out='0')     -- assert statement tests the boolean condition. If
+      -- this is false, it outputs a message containing the reopirt string to
+      -- the simulator screen.
+      report "Fail 0/0"   -- report <message_string> [severity <severity_level>];
+      severity error;     -- The severity level may be defined as note, warning,
+    -- error or failure Level failure normally aborts the simulation.
+
     a_in <= '0';
     b_in <= '1';
     wait for 1 ns;
-    assert(q_out='1') report "Fail 0/1" severity error;
+    assert(q_out='1')
+      report "Fail 0/1"
+      severity error;
 
     a_in <= '1';
     b_in <= 'X';
     wait for 1 ns;
-    assert(q_out='1') report "Fail 1/X" severity error;
+    assert(q_out='1')
+      report "Fail 1/X"
+      severity error;
 
     a_in <= '1';
     b_in <= '1';
     wait for 1 ns;
-    assert(q_out='1') report "Fail 1/1" severity error;
-    
+    assert(q_out='1')
+      report "Fail 1/1"
+      severity error;
+
     -- Clear inputs
     a_in <= '0';
     b_in <= '0';
 
-    assert false report "Test done." severity note;
+    assert false
+      report "Test done."
+      severity note;
     wait;
-  end process;
-end tb;
+
+  end process process_tb;
+
+end architecture tb;
