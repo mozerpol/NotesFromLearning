b440b2db2c13c9024cceb9d061bab95baaa029da
mozerpol <krzmozer@student.pg.edu.pl>
2021-11-22 22:34:43 +0100

    Add comments and fix stylistic errors

--- learningVHDL/XORgate/testbench.vhd
+++ learningVHDL/XORgate/testbench.vhd
@@ -1,42 +1,54 @@
 library IEEE;
-use IEEE.std_logic_1164.all;
+  use IEEE.std_logic_1164.all;
 
-entity testbench is 
-  end testbench;
+entity testbench is
+end entity testbench;
 
-  architecture tb of testbench is
-    component xor_gate is
-      port(
-        a: in std_logic;
-        b: in std_logic;
-        c: out std_logic;
-      );
-    end component;
+architecture tb of testbench is
 
-      signal a_in, b_in, c_out: std_logic;
+  component xor_gate is
+    port (
+      a : in    std_logic;
+      b : in    std_logic;
+      c : out   std_logic
+    );
+  end component;
 
+  signal a_in  : std_logic;
+  signal b_in  : std_logic;
+  signal c_out : std_logic;
+
+begin
+
+  dut : component xor_gate
+    port map (
+      a => a_in,
+      b => b_in,
+      c => c_out
+    );
+
+  process_tb : process is
   begin
 
-    DUT: xor_gate port map(a_in, b_in, c_out);
+    a_in <= '0';
+    b_in <= '0';
+    wait for 1 ns;
+    a_in <= '0';
+    b_in <= '1';
+    wait for 1 ns;
+    a_in <= '1';
+    b_in <= '0';
+    wait for 1 ns;
+    a_in <= '1';
+    b_in <= '1';
+    wait for 1 ns;
+    a_in <= 'X';
+    wait for 1 ns;
+    -- Clear inputs
+    a_in <= '0';
+    b_in <= '0';
+    wait;
 
-process begin
-  a_in <= '0';
-  b_in <= '0';
-  wait for 1 ns;
-  a_in <= '0';
-  b_in <= '1';
-wait for 1 ns;
-  a_in <= '1';
-  b_in <= '0';
-wait for 1 ns;
-  a_in <= '1';
-  b_in <= '1';
-wait for 1 ns;
-  a_in <= 'X';
-wait for 1 ns;
-  -- Clear inputs
-  a_in <= '0';
-  b_in <= '0';
-wait;
-end process;
-  end tb;
+  end process process_tb;
+
+end architecture tb;
