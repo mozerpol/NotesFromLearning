22f9fd494c092ea49ad6baeea1cf0894a768c14c
mozerpol <krzmozer@student.pg.edu.pl>
2021-12-02 22:49:41 +0100

    Add simple possibility of incrementing counter

--- learningVHDL/simpleCounter/design.vhd
+++ learningVHDL/simpleCounter/design.vhd
@@ -1,5 +1,6 @@
 library ieee;
   use ieee.std_logic_1164.all;
+  use IEEE.std_logic_unsigned.all;
 
 entity simplecounter is
   generic (
@@ -7,18 +8,25 @@ entity simplecounter is
   );
   port (
     clk : in    std_logic;
-    q   : out   std_logic
+    q   : out   std_logic := '0'
   );
 end entity simplecounter;
 
 architecture rtl of simplecounter is
+
+  signal counter : std_logic_vector(10 downto 0) := "00000000000";
 
 begin
 
   simplecounter_process : process (clk) is
   begin
 
-    if (clk'event and clk = '1') then
+    if (clk'event and clk = '1' and count_up_to /= counter) then
+      counter <= counter + 1;
+    end if;
+
+    if (count_up_to = counter) then
+      q <= '1';
     end if;
 
   end process simplecounter_process;
