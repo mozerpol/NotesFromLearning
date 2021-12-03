21a16a3cb21105038e5a67552251e27a0329efff
mozerpol <krzmozer@student.pg.edu.pl>
2021-12-03 15:43:11 +0100

    Add generic to component

--- learningVHDL/simpleCounter/testbench.vhd
+++ learningVHDL/simpleCounter/testbench.vhd
@@ -8,6 +8,9 @@ end entity testbench;
 architecture tb of testbench is
 
   component simplecounter is
+    generic (
+      count_up_to : integer
+      );
     port (
       clk : in    std_logic;
       q   : out   std_logic
@@ -15,7 +18,7 @@ architecture tb of testbench is
   end component;
 
   signal   clk_in      : std_logic := '0';
-  constant count_up_to : integer := 12;
+  constant count_up_to : integer := 10;
   signal   q_out       : std_logic;
 
 begin
@@ -32,7 +35,7 @@ begin
   tb_process : process is
   begin
 
-    wait for 10 ns; stop;
+    wait for 100 ns; stop;
 
   end process tb_process;
 
