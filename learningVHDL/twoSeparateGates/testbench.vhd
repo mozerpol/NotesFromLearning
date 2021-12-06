006dd4b6acea4ea9effb9c94ac158ffca7d7eb1e
mozerpol <krzmozer@student.pg.edu.pl>
2021-12-06 14:53:15 +0100

    Add component and bind it with signals

--- learningVHDL/twoSeparateGates/testbench.vhd
+++ learningVHDL/twoSeparateGates/testbench.vhd
@@ -1,18 +1,46 @@
 library ieee;
-use ieee.std_logic_1164.all;
-use std.env.stop;
+  use ieee.std_logic_1164.all;
+  use std.env.stop;
 
 entity testbench is
-    end entity testbench;
+end entity testbench;
 
-    architecture tb of testbench is 
-    component and_area is 
+architecture tb of testbench is
+
+  component gates_area is
     port (
-        in1 : in std_logic;
-        in2 : in std_logic;
-        out1_and : out std_logic
+      in1    : in    std_logic;
+      in2    : in    std_logic;
+      in3    : in    std_logic;
+      out_or : out   std_logic
     );
-    end component;
-    begin
+  end component;
 
-    end architecture tb;
+  signal in1    : std_logic;
+  signal in2    : std_logic;
+  signal in3    : std_logic;
+  signal out_or : std_logic;
+
+begin
+
+  dut : component gates_area
+    port map (
+      in1    => in1,
+      in2    => in2,
+      in3    => in3,
+      out_or => out_or
+    );
+
+  tb_process : process is
+  begin
+
+    wait for 1 ns;
+    in1 <= '0';
+    in2 <= '0';
+    in3 <= '0';
+    wait for 1 ns;
+    stop;
+
+  end process tb_process;
+
+end architecture tb;
