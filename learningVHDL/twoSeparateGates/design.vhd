c4aab51b1c86174e0a092cfe26cebdba63efd985
mozerpol <krzmozer@student.pg.edu.pl>
2021-12-06 14:52:53 +0100

    Fix a typos

--- learningVHDL/twoSeparateGates/design.vhd
+++ learningVHDL/twoSeparateGates/design.vhd
@@ -15,11 +15,13 @@ begin
   and_process : process is
   begin
 
-    out1_and <= in1 & in2;
+    out1_and <= in1 and in2;
 
   end process and_process;
 
 end architecture rtl;
+
+-- //////////////////////////////////////////////////////
 
 library ieee;
   use ieee.std_logic_1164.all;
@@ -33,10 +35,30 @@ end entity gates_area;
 
 architecture rtl2 of gates_area is
 
+  component and_area is
+    port (
+      in1      : in    std_logic;
+      in2      : in    std_logic;
+      out1_and : out   std_logic
+    );
+  end component and_area;
+
+  signal internal_wire : std_logic;
+
 begin
 
+  component_and_area : component and_area
+    port map (
+      in1      => in1,
+      in2      => in2,
+      out1_and => internal_wire
+    );
+
   gates_process : process is
+
   begin
+
+    out_or <= in3 or internal_wire;
 
   end process gates_process;
 
