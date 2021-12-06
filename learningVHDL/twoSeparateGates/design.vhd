6b856159716db1aa4ba45e913ab7c53892182bc3
mozerpol <krzmozer@student.pg.edu.pl>
2021-12-06 13:49:53 +0100

    Add second entity gates_area

--- learningVHDL/twoSeparateGates/design.vhd
+++ learningVHDL/twoSeparateGates/design.vhd
@@ -15,6 +15,29 @@ begin
   and_process : process is
   begin
 
+    out1_and <= in1 & in2;
+
   end process and_process;
 
 end architecture rtl;
+
+library ieee;
+  use ieee.std_logic_1164.all;
+entity gates_area is port (
+    in1    : in    std_logic;
+    in2    : in    std_logic;
+    in3    : in    std_logic;
+    out_or : out   std_logic
+  );
+end entity gates_area;
+
+architecture rtl2 of gates_area is
+
+begin
+
+  gates_process : process is
+  begin
+
+  end process gates_process;
+
+end architecture rtl2;
