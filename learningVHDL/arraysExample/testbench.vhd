374a609fd322539c0d6a297a1546c3e2c1d2a6e5
mozerpol <krzmozer@student.pg.edu.pl>
2021-12-13 14:49:21 +0100

    Add accessing to the constrained arrays

--- learningVHDL/arraysExample/testbench.vhd
+++ learningVHDL/arraysExample/testbench.vhd
@@ -18,8 +18,9 @@ architecture tb of testbench is
   -- 1: [1, 0]
   -- 2: [1, 0]
   -- 3: [1, 0]
-  type mul_dim_1 is array (0 to 3) of std_logic_vector (1 downto 0);
-  signal array2 : mul_dim_1;
+  type mul_dim_1 is array (0 to 3) of std_logic_vector (1 downto 0); -- define
+  -- the type of array.
+  signal array2 : mul_dim_1; -- array2 is a mul-dim array of std_logic type.
 
   -- Create constrained multi-dim array of two bits, it's looks like: 
   -- 0: [0, 1] 
@@ -28,21 +29,37 @@ architecture tb of testbench is
   -- 3: [0, 1]
   -- This array is almost the same as above, but in this case we must use
   -- integer type. I'm not sure that this example is synthesizable. 
-  type mul_dim_2 is array (0 to 3, 0 to 1) of integer;
+  type mul_dim_2 is array (0 to 3, 0 to 1) of integer ; 
   signal array3 : mul_dim_2;
-
-  -- Creates an unconstrained array (MUST be constrained when defined!)
-
-  type t_integer_array is array (integer range <>) of integer;
 
 begin
 
   tb_process : process is
 
-    variable r_integers : t_integer_array(0 to 15);
-
   begin
 
+    -- Accessing to the: "type one_dim_8 is array (0 to 7) of std_logic;"
+    array1(0) <= '1';
+    array1(1) <= '0';
+    array1(2) <= '1';
+    array1(3 to 5) <= "010";
+
+    -- Accessing to the: 
+    -- "type mul_dim_1 is array (0 to 3) of std_logic_vector (1 downto 0);" 
+    array2(0)(0) <= '0';
+    array2(0)(1) <= '1';
+    array2(1)(0) <= '0';
+    array2(1)(1) <= '1';
+    array2(2)(1 downto 0) <= "00";
+    -- array2(3,0) <= '1'; It's illegal
+
+    -- Accessing to the: "type mul_dim_2 is array (0 to 3, 0 to 1) of integer;"
+    array3(0,0) <= 99;
+    array3(0,1) <= 23;
+    array3(1,0) <= 12;
+    array3(1,1) <= 0;
+
+    wait for 10 ns;
     stop;
 
   end process tb_process;
