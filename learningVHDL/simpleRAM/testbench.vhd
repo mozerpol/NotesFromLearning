d8c1a06eb83ebb4ab9f47c361a49fa6a1b918669
mozerpol <krzmozer@student.pg.edu.pl>
2021-12-14 21:27:50 +0100

    Bind connection between test and design

--- learningVHDL/simpleRAM/testbench.vhd
+++ learningVHDL/simpleRAM/testbench.vhd
@@ -7,17 +7,38 @@ end entity testbench;
 
 architecture tb of testbench is
 
+  component simpleram is
+    port (
+      clk  : in    std_logic;
+      we   : in    std_logic;
+      re   : in    std_logic;
+      addr : in    std_logic_vector(11 downto 0);
+      data : inout std_logic_vector(7 downto 0)
+    );
+  end component;
+
   signal clk_in : std_logic := '0';
+  signal we     : std_logic;
+  signal re     : std_logic;
+  signal addr   : std_logic_vector(11 downto 0);
+  signal data   : std_logic_vector(7 downto 0);
 
 begin
 
   dut : component simpleram
     port map (
-      clk => clk_in
+      clk  => clk_in,
+      we   => we,
+      re   => re,
+      addr => addr,
+      data => data
     );
 
   tb_process : process is
   begin
+
+    wait for 10 ns;
+    stop;
 
   end process tb_process;
 
