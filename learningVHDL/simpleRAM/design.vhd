25cd42e766f9ebc95c0d82871783a65ecd55a672
mozerpol <krzmozer@student.pg.edu.pl>
2021-12-15 14:46:23 +0100

    Add memory and operation depending on the activity

--- learningVHDL/simpleRAM/design.vhd
+++ learningVHDL/simpleRAM/design.vhd
@@ -8,8 +8,8 @@ entity simpleram is generic (
   );
   port (
     clk  : in    std_logic;
-    we   : in    std_logic;
-    re   : in    std_logic;
+    we   : in    std_logic; -- Write enable
+    re   : in    std_logic; -- Read enable
     addr : in    std_logic_vector(w - 1 downto 0);
     data : inout std_logic_vector(7 downto 0)
   );
@@ -17,10 +17,23 @@ end entity simpleram;
 
 architecture rtl of simpleram is
 
+  type mem_type is array (0 to addresssize - 1) of std_logic_vector(wordsize - 1 downto 0);
+
+  signal memory   : mem_type;
+  signal addr_int : integer := 0;
+
 begin
 
   simpleram_process : process (clk) is
   begin
+
+    if (clk'event and clk = '1') then
+      if ((re and not(we)) = '1') then
+        data <= memory(addr_int);
+      else
+        data <= "ZZZZZZZZ";
+      end if;
+    end if;
 
   end process simpleram_process;
 
