6eb28c72ed35de1161249cd79590779e9fe227ee
mozerpol <krzmozer@student.pg.edu.pl>
2023-01-12 16:57:39 +0100

    Add tcl scripts for compiling design, test and show waveforms

---
+++ learningVHDL/tclScripts/simplegate_pkg.vhd
@@ -0,0 +1,12 @@
+library ieee;
+  use ieee.std_logic_1164.all;
+  use IEEE.std_logic_unsigned.all;
+  use IEEE.math_real.all;
+  
+ package simplegate_pkg is
+    constant C_TEST : std_logic_vector(1 downto 0) := "11";
+ end;
+ 
+ package body simplegate_pkg is
+ 
+ end package body;
