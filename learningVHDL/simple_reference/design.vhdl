2fb69e6ddbb8b17d02ea21c2cb2cfe11b8b7aa10
mozerpol <krzmozer@student.pg.edu.pl>
2024-10-18 13:06:58 +0200

    Fix indents

--- learningVHDL/simple_reference/design.vhdl
+++ learningVHDL/simple_reference/design.vhdl
@@ -1,23 +1,23 @@
 library ieee; -- IEEE 1164 standard describes the definitions of logic values
-  use ieee.std_logic_1164.all;
-  use ieee.numeric_std.all;
+   use ieee.std_logic_1164.all;
+   use ieee.numeric_std.all;
 
 entity and_gate is port ( -- entity is used to describe a hardware module
-    a : in    std_logic;
-    b : in    std_logic;
-    c : out   std_logic
-  );
+   a : in    std_logic;
+   b : in    std_logic;
+   c : out   std_logic
+);
 end entity and_gate;
 
 architecture rtl of and_gate is
 
 begin
 
-  process_and : process (a, b) is
-  begin             -- Code inside the process statement is executed sequentially
+   process_and : process (a, b) is
+   begin          -- Code inside the process statement is executed sequentially
 
-    c <= a and b;
+      c <= a and b;
 
-  end process process_and;
+   end process process_and;
 
 end architecture rtl;
