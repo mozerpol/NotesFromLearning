441bb3aae5ee033f2b56ff884acfde010fe272b8
mozerpol <krzmozer@student.pg.edu.pl>
2025-05-10 17:52:53 +0200

    Modify

--- STM32_Arm_Programming_book/7_segment/Src/main.c
+++ STM32_Arm_Programming_book/7_segment/Src/main.c
@@ -1,3 +1,8 @@
+/*
+ * When running this example, common-cathode 5611AH displays were used, so PORTB 
+ * (the one that turns the displays on/off) must be set low state.
+*/
+
 #include <stdint.h>
 
 // Base addresses for peripherals
@@ -7,10 +12,11 @@
 // RCC register
 #define RCC_AHB1ENR    (*(volatile uint32_t *)(RCC_BASE + 0x30))
 // GPIOB registers
-#define GPIOB_MODER    (*(volatile uint32_t *)(GPIOB_BASE + 0x00)) // output
+#define GPIOB_MODER    (*(volatile uint32_t *)(GPIOB_BASE + 0x00))
+#define GPIOB_PUPDR    (*(volatile uint32_t *)(GPIOB_BASE + 0x0C))
 #define GPIOB_BSRR     (*(volatile uint32_t *)(GPIOB_BASE + 0x18))
 // GPIOC registers
-#define GPIOC_MODER    (*(volatile uint32_t *)(GPIOC_BASE + 0x00)) // output
+#define GPIOC_MODER    (*(volatile uint32_t *)(GPIOC_BASE + 0x00))
 #define GPIOC_ODR      (*(volatile uint32_t *)(GPIOC_BASE + 0x14))
 
 
@@ -19,24 +25,28 @@ void delayMs(int n);
 
 int main(void)
 {
-   // Enable the clock for GPIOA and GPIOC
+   // Enable the clock for GPIOB and GPIOC
    RCC_AHB1ENR |= (1 << 1); // Enable GPIOB clock
    RCC_AHB1ENR |= (1 << 2); // Enable GPIOC clock
    // Configure PB0 and PB1 as a output
-   GPIOB_MODER |= 0x5; // 0101
+   GPIOB_MODER |= 0x5; // 0b0101
+   // Connect PB0 and PB1 to pull-down resistor
+   GPIOB_PUPDR |= 0xA; // 0b1010
    // Configure PC0-PC7 as a output
    GPIOC_MODER |= 0x5555; // 0101010101010101
 
    for(;;)
    {
-      GPIOC_ODR = 0x0007; // Display tens digit
-      GPIOB_BSRR = 0x00010000; // Deselect ones digit, PB0
-      GPIOB_BSRR = 0x00000002; // Select tens digit, PB1
-      delayMs(8);
-      GPIOC_ODR = 0x006D; // Display ones digit
-      GPIOB_BSRR = 0x00020000; // Deselect tens digit, PB0
-      GPIOB_BSRR = 0x00000001; // Select ones digit, PB1
-      delayMs(8);
+      // Set high state on PC0, PC1 and PC2
+      GPIOC_ODR = 0x0007; // 0b111
+      GPIOB_BSRR = 0x00010000; // Set low state on PB0
+      GPIOB_BSRR = 0x00000002; // Set high state on PB1
+      delayMs(5);
+      // Set high state on PC0, PC2, PC3, PC5 and PC6 
+      GPIOC_ODR = 0x006D; // 0b1101101
+      GPIOB_BSRR = 0x00020000; // Set low state on PB1
+      GPIOB_BSRR = 0x00000001; // Set high state on PB0
+      delayMs(5);
    }
 }
 
